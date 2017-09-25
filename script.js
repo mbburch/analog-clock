9cf62c99225197773d149a2e8b846bcd0fb7e579
MB Burch <marybethburch@gmail.com>
2017-09-25 13:30:27 -0600

    Add css and js to display rotating clock hands

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ class Clock {
     this.secondHand = document.querySelector('.second-hand');
     this.pauseButton = document.querySelector('#pause');
     this.resumeButton = document.querySelector('#resume');
+
+    this.runClock = setInterval(() => this.setTime(), 1000);
+  }
+
+  setTime() {
+    const time = new Date();
+
+    const seconds = time.getSeconds();
+    const secDegrees = ((seconds/60) * 360) + 90;
+    this.secondHand.style.transform = `rotate(${secDegrees}deg)`;
+
+    const minutes = time.getMinutes();
+    const minDegrees = ((minutes/60) * 360) + ((seconds/60) * 6) + 90;
+    this.minuteHand.style.transform = `rotate(${minDegrees}deg)`;
+
+    const hour = time.getHours();
+    const hourDegrees = ((hour/12) * 360) + ((minutes/60) * 30) + 90;
+    this.hourHand.style.transform = `rotate(${hourDegrees}deg)`;
   }
 }
 
